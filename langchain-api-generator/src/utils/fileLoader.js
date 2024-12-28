c3a164247875d946207b1733ed325cc104ce7c49
Ren Munetsuna <munetsuna.r@x-point-1.net>
2024-12-29 05:03:04 +0900

    修正

--- langchain-api-generator/src/utils/fileLoader.js
+++ langchain-api-generator/src/utils/fileLoader.js
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 export const fileLoader = (filePath) => {
   try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
     const targetPath = path.resolve(__dirname, filePath);
     return fs.readFileSync(targetPath, 'utf8');
   } catch (error) {
