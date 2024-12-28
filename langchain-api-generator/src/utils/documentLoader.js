15b684f3752e250e9350d599a71d9289e99e246b
Ren Munetsuna <munetsuna.r@x-point-1.net>
2024-12-29 04:04:03 +0900

    パス修正

--- langchain-api-generator/src/utils/documentLoader.js
+++ langchain-api-generator/src/utils/documentLoader.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const WORKSPACE_ROOT = path.resolve(__dirname, '../../../');
+const WORKSPACE_ROOT = path.resolve(__dirname, '../../');
 
 /**
  * ドキュメントファイルを読み込む
